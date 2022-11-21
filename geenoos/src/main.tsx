60590506078d37f2fd9fce6fbd238e9c5a246a44
최진우 <freiya1002@naver.com>
2022-11-21 17:38:26 +0900

    [Init] : Create Directory Architecture

--- geenoos/src/main.tsx
+++ geenoos/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { RecoilRoot } from "recoil"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 )
